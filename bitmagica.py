998d034d3ce7db3e1a01424868777f43e2f770bf
Richard_ <richyrjrj@gmail.com>
2026-01-03 15:52:21 +0300

    Add files via upload

---
+++ bitmagica.py
@@ -0,0 +1,30 @@
+
+ones = "1"                  # storage for one
+zeros = "0"                 #  storage for zero     
+all =  zeros + ones          # concenate for all 
+
+
+while all:                    # main loop 
+  print(*all,*all*4231)       #displaying output
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
